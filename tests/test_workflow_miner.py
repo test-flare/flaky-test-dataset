8a0f6129648d2d8e12c76b4aa4571e27ca16411c
Michael Foster <m.foster@sheffield.ac.uk>
2026-07-01 14:04:38 +0100

    fixed test workflow miner

--- tests/test_workflow_miner.py
+++ tests/test_workflow_miner.py
@@ -29,6 +29,6 @@ def test_get_failed_tests_from_logs():
         github_token=os.getenv("GITHUB_TOKEN"), repo_owner="home-assistant", repo_name="core", base_branch="dev"
     )
     with open("tests/resources/logs_71135566034.zip", "rb") as f:
-        assert workflow_miner.get_failed_tests_from_logs(f.read()) == [
+        assert list(workflow_miner.get_failed_tests_from_logs(f.read())) == [
             "tests/components/device_tracker/test_entity.py::test_attr_location_name_deprecation_warning"
         ]
