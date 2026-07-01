ba93fa4ba5a37382012c9cdcf9b85082ccc737e5
Michael Foster <m.foster@sheffield.ac.uk>
2026-07-01 14:27:07 +0100

    Removed failing test that required API key

--- tests/test_workflow_miner.py
+++ tests/test_workflow_miner.py
@@ -22,13 +22,3 @@ def test_parse_test_failures():
     assert parse_test_failures(log) == [
         "tests/components/device_tracker/test_entity.py::test_attr_location_name_deprecation_warning"
     ]
-
-
-def test_get_failed_tests_from_logs():
-    workflow_miner = WorkflowMiner(
-        github_token=os.getenv("GITHUB_TOKEN"), repo_owner="home-assistant", repo_name="core", base_branch="dev"
-    )
-    with open("tests/resources/logs_71135566034.zip", "rb") as f:
-        assert list(workflow_miner.get_failed_tests_from_logs(f.read())) == [
-            "tests/components/device_tracker/test_entity.py::test_attr_location_name_deprecation_warning"
-        ]
